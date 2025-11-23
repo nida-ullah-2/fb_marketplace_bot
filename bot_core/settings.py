cd9e0645acba71f4f809d6c9d3902f7fd52c1e56
Nida Ullah <nidaullah2002.2@gmail.com>
2025-11-22 22:40:05 -0500

    fresh commit if need then restore of pull

--- bot_core/settings.py
+++ bot_core/settings.py
@@ -76,6 +76,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'corsheaders.middleware.CorsMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
 ROOT_URLCONF = 'bot_core.urls'
 
 TEMPLATES = [
